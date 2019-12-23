b4e4df09e50d7026a010fde95688f676927083a8
Ekow Abaka Ainooson <jainooson@gmail.com>
2019-12-22 19:27:33 -0600

    Ensuring webps load over jpegs

--- templates/responsive_images.tpl.php
+++ templates/responsive_images.tpl.php
@@ -1,8 +1,8 @@
 <figure>
     <picture>
         <?php foreach($sources as $source): ?>
-        <source srcset="<?= $site_path . $source['src_jpeg'] ?>" media="(max-width: <?= $source['max_width'] ?>px)" />
         <source srcset="<?= $site_path . $source['src_webp'] ?>" media="(max-width: <?= $source['max_width'] ?>px)" />
+        <source srcset="<?= $site_path . $source['src_jpeg'] ?>" media="(max-width: <?= $source['max_width'] ?>px)" />
         <?php endforeach; ?>
         <img src="<?= $site_path . $image_path ?>" alt="<?= $alt ?>" />
     </picture>
